b6ca0459a7721beb71c014bf5155a8d8a92dfbfb
cgoldberg <cgoldberg>
2004-06-14 14:46:13 +0000

    added version number for 1.10

--- webinjectgui.pl
+++ webinjectgui.pl
@@ -33,7 +33,7 @@ $| = 1; #don't buffer output to STDOUT
 
 
  
-$mw = MainWindow->new(-title            => 'WebInject - HTTP Test Tool    (version .95)',
+$mw = MainWindow->new(-title            => 'WebInject - HTTP Test Tool    (version 1.10)',
                       -bg               => '#666699',
                       -takefocus        => '1'  #start on top
                       );
