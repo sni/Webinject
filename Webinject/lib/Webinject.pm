4f086b8540f09f64303d82ba7b2e5fa578e6b326
Sven Nierlein <sven@nierlein.de>
2012-01-03 09:39:03 +0100

    fixed nagios epn support or check_webinject

--- Webinject/lib/Webinject.pm
+++ Webinject/lib/Webinject.pm
@@ -31,7 +31,7 @@ use Error qw(:try);             # for web services verification (you may comment
 use Data::Dumper;               # dump hashes for debugging
 use File::Temp qw/ tempfile /;  # create temp files
 
-our $VERSION = '1.70';
+our $VERSION = '1.71';
 
 =head1 NAME
 
